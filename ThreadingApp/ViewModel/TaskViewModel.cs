a61b5a63e8de0fe82663878afe6c5da06776a4b8
narender.kunta <narender.kunta@NARENDERKUNTA>
2020-01-08 15:36:51 +0530

    removed unwanted comments

--- ThreadingApp/ViewModel/TaskViewModel.cs
+++ ThreadingApp/ViewModel/TaskViewModel.cs
@@ -13,7 +13,7 @@ namespace ThreadingApp.ViewModel
 {
     public class TaskViewModel : BindableBase
     {
-        //TaskScheduler uiScheduler;
+
         private static Queue<Action> MyQueue = new Queue<Action>();
         private static EventWaitHandle newtaskavailable = new AutoResetEvent(false);
         public List<Action> TaskList = new List<Action>();
@@ -58,7 +58,6 @@ namespace ThreadingApp.ViewModel
             }
         }
 
-
         private double _currentProgress1;
         public double CurrentProgress1
         {
@@ -68,7 +67,6 @@ namespace ThreadingApp.ViewModel
                 SetProperty(ref _currentProgress1, value);
             }
         }
-
         private double _currentProgress2;
         public double CurrentProgress2
         {
@@ -90,7 +88,6 @@ namespace ThreadingApp.ViewModel
         }
 
         private string _taskNameList;
-
         public string TaskNameList
         {
             get { return _taskNameList; }
@@ -104,16 +101,12 @@ namespace ThreadingApp.ViewModel
             get { return _taskNameList1; }
             set { SetProperty(ref _taskNameList1, value); RaisePropertyChanged(); }
         }
-
-
         public ICommand ButtonTask1Command { get; set; }
         public ICommand ButtonTask2Command { get; set; }
         public ICommand ButtonTask3Command { get; set; }
         public ICommand StartTaskCommand { get; set; }
         public ICommand AddTaskCommand { get; set; }
         public ICommand ClearTaskCommand { get; set; }
-
-
         public TaskViewModel()
         {
             ButtonTask1Command = new DelegateCommand(RunTask1);
@@ -121,27 +114,26 @@ namespace ThreadingApp.ViewModel
             ButtonTask3Command = new DelegateCommand(RunTask3);
             StartTaskCommand = new DelegateCommand(ExecuteStartTask);
             AddTaskCommand = new DelegateCommand(ExecuteAddTask);
-            ClearTaskCommand = new DelegateCommand(ExecuteClearTask);
-            //TaskNameList = new List<string>();
+            ClearTaskCommand = new DelegateCommand(ExecuteClearTask);            
         }
 
         public void RunTask1()
         {
-            TaskList.Add(Task1);
-            TaskNameList += Environment.NewLine;
-            TaskNameList += "Task 1 Added.";
+            this.TaskList.Add(Task1);
+            this.TaskNameList += Environment.NewLine;
+            this.TaskNameList += "Task 1 Added.";
         }
         public void RunTask2()
         {
-            TaskNameList += Environment.NewLine;
-            TaskNameList += "Task 2 Added.";
-            TaskList.Add(Task2);
+            this.TaskNameList += Environment.NewLine;
+            this.TaskNameList += "Task 2 Added.";
+            this.TaskList.Add(Task2);
         }
         public void RunTask3()
         {
-            TaskNameList += Environment.NewLine;
-            TaskNameList += "Task 3 Added.";
-            TaskList.Add(Task3);
+           this.TaskNameList += Environment.NewLine;
+           this.TaskNameList += "Task 3 Added.";
+           this.TaskList.Add(Task3);
         }
 
         public void ExecuteAddTask()
@@ -162,11 +154,11 @@ namespace ThreadingApp.ViewModel
 
         private void ExecuteClearTask()
         {
-            CurrentProgress1 = CurrentProgress2 = CurrentProgress3 = 0;
-            FinishedTask1 = FinishedTask2 = FinishedTask3 = false;
+            this.CurrentProgress1 = CurrentProgress2 = CurrentProgress3 = 0;
+            this.FinishedTask1 = FinishedTask2 = FinishedTask3 = false;
         }
 
-        private void Task1()
+        private void Task1() 
         {
             if (!FinishedTask1)
             {
@@ -208,10 +200,8 @@ namespace ThreadingApp.ViewModel
               }).Wait();
             CurrentProgress1 = 0;
             TaskNameList1 += Environment.NewLine;
-            TaskNameList1 += "Task 1 is Completed.";
-          
+            TaskNameList1 += "Task 1 is Completed.";          
         }
-
         public void PrepareTask2()
         {
             Task.Factory.StartNew(
@@ -223,12 +213,9 @@ namespace ThreadingApp.ViewModel
                      CurrentProgress2 = i;
                  }
              }).Wait();
-
             CurrentProgress2 = 0;
             TaskNameList1 += Environment.NewLine;
-            TaskNameList1 += "Task 2 is Completed.";
-
-           
+            TaskNameList1 += "Task 2 is Completed.";           
         }
 
         public void PrepareTask3()
@@ -242,11 +229,9 @@ namespace ThreadingApp.ViewModel
                      CurrentProgress3 = i;
                  }
              }).Wait();
-
             CurrentProgress3 = 0;
             TaskNameList1 += Environment.NewLine;
-            TaskNameList1 += "Task 3 is Completed.";
-            
+            TaskNameList1 += "Task 3 is Completed.";            
         }
 
         public async void ExecuteTasks()
@@ -275,8 +260,6 @@ namespace ThreadingApp.ViewModel
                 }
             }
         }
-
-
 
     }
 }
